0861fb43b6075e7d2adf7f3941d9c31ec7fa0952
oznksoy <oznksoy@gmail.com>
2024-03-17 15:58:53 +0100

    [UPDATE]: gradle structural update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,25 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.oznksoy.football"
 version = "1.0-SNAPSHOT"
+
+val springBootVersion = "3.2.3"    // Spring boot version
+val junitBOMVersion = "5.10.2"   // Collective junit project dependency alignments
+val mockitoVersion = "5.11.0"   // Mockito juniper
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    implementation("org.springframework.boot:spring-boot-starter-web:${springBootVersion}")
+    testImplementation(platform("org.junit:junit-bom:${junitBOMVersion}"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation(platform("org.mockito:mockito-junit-jupiter:5.11.0"))
+    testImplementation(platform("org.mockito:mockito-junit-jupiter:${mockitoVersion}"))
     testImplementation("org.mockito:mockito-junit-jupiter")
 }
 
