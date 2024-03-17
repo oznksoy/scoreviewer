a2a5b7d87e2767ae2f70010ebec04d6836e2f3db
oznksoy <oznksoy@gmail.com>
2024-03-17 13:30:52 +0100

    [ADD]: gradle aspect is added

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "com.oznksoy.football"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform("org.mockito:mockito-junit-jupiter:5.11.0"))
+    testImplementation("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
